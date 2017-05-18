d7a3ccad3d2e15a3142d63829ec069134f2be393
Avenue Code Recruitment <ac.recruitment@avenuecode.com>
2017-05-18 18:03:53 +0000

    Using `freegeoip.net` API to retrieve IP

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,25 +1,33 @@
+// ================================================
+// Note to candidate:
+//	This code has bad practices and defects
+//	feel free to replace it with a better solution
+// ================================================
+
+var API_HOST = "freegeoip.net";
+
 function updateLocationDetails(data){
 	var now = new Date();
 
-	$("#location_query").html(data.query);
-	$("#location_country").html(data.country);
-	$("#location_regionName").html(data.regionName);
+	$("#location_query").html(data.ip);
+	$("#location_country").html(data.country_name);
+	$("#location_regionName").html(data.region_name);
 	$("#location_city").html(data.city);
-	$("#location_timezone").html(data.timezone);
-	$("#location_lat").html(data.lat);
-	$("#location_lon").html(data.lon);
+	$("#location_zip_code").html( data.zip_code);
+	$("#location_lat").html(data.latitude);
+	$("#location_lon").html(data.longitude);
 
 	$("table").removeClass("empty");
 	$(".help").click(function(e){
 		var fieldName = $(e.currentTarget).closest('tr').find('.field_name').text();
-		alert("This is your " + fieldName + " from ISP " + data.isp + " at " + now);
+		alert( "This is your " + fieldName + " according to " + API_HOST + " at " + now );
 	});
 }
 
 function getMyLocation() {
 	$.ajax({
 		type : 'GET',
-		url : 'http://ip-api.com/json/',
+		url : 'http://' + API_HOST + '/json/',
 		success : function(response){
 			updateLocationDetails(response);
 		}
@@ -28,13 +36,13 @@ function getMyLocation() {
 
 function resetLocationDetails() {
 	updateLocationDetails({
-		query: "0.0.0.0",
-		country: "",
-		regionName: "",
+		ip: "0.0.0.0",
+		country_name: "",
+		region_name: "",
 		city: "",
-		timezone: "",
-		lat: "",
-		lon: ""
+		zip_code: "",
+		latitude: "",
+		longitude: ""
 	});
 	$("table").addClass("empty");
 }
@@ -49,13 +57,13 @@ function initializePage(){
 	$("#mainContent").html(window.indexTemplate());
 	$("#geoLocationContainer").html(window.locationTemplate({
 		id: 0,
-		query: "0.0.0.0",
-		country: "",
-		regionName: "",
+		ip: "0.0.0.0",
+		country_name: "",
+		region_name: "",
 		city: "",
-		timezone: "",
-		lat: "",
-		lon: ""
+		zip_code: "",
+		latitude: "",
+		longitude: ""
 	}));
 }
 
